f0b4731bc9a9238f8b02d8b625ef8c056a69dd4c
aashish47 <aashish27nov@gmail.com>
2025-04-13 09:14:16 +0530

    Update bubble_sort.py

--- sorting_and_searching/bubble_sort.py
+++ sorting_and_searching/bubble_sort.py
@@ -1,14 +1,26 @@
-def bubble_sort(a: list[int]):
-    for i in range(len(a)):
-        for j in range(len(a) - 1 - i):
-            if a[j] > a[j + 1]:
-                a[j], a[j + 1] = a[j + 1], a[j]
+def bubble_sort(arr: list[int]):
+    n = len(arr)
+    print(f"\nsize: {n}\n")
+
+    for i in range(n - 1):
+        swapped = False
+
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if not swapped:
+            return
+
+        print(f"After {i + 1} pass: {arr}")
 
 
 def main():
-    a = [33, 2, 4, 5, 2, 1, 3, 44, 5, 33, 2]
-    bubble_sort(a)
-    print(a)
+    arr = [33, 2, 4, 5, 2, 1, 3, 44, 5, 33, 2]
+    print(f"\nInitial array: {arr}")
+    bubble_sort(arr)
+    print(f"\nSorted array: {arr}")
 
 
 if __name__ == "__main__":
